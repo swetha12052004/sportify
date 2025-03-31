2219484526023ed59d338d8e25c3e337c9006539
swetha12052004 <swethaswe259@gmail.com>
2025-03-31 16:30:52 +0530

    final 1 change

--- my-app/src/project/Home.jsx
+++ my-app/src/project/Home.jsx
@@ -6,13 +6,6 @@ import img3 from "../images/back4.jpg";
 
 const images = [img1, img2, img3];
 
-
-// const images = [
-//   "../images/img3.jpg",  // Ensure images are inside the 'public/' folder
-//   "../images/fine.jpg",
-//   "../images/back4.png",
-// ];
-
 const App = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
@@ -26,20 +19,67 @@ const App = () => {
 
   return (
     <div>
-      {/* Navbar */}
-      {/* <nav className="navbar"> */}
-      <nav>
-        <h1 className="logo">SPORTIFY</h1>
-        <div className="navbar">
+    <nav className="navbar">
+        <div className="logo">
+          <h1>SPORTIFY</h1>
+        </div>
         <ul className="nav-links">
+          <li className="dropdown">
+            <button className="dropbtn">All Sports</button>
+            <div className="dropdown-content">
+              <a href="#">Football</a>
+              <a href="#">Hockey</a>
+              <a href="#">Cricket</a>
+              <a href="#">Basketball</a>
+              <a href="#">Badminton</a>
+              <a href="#">Swimming</a>
+              <a href="#">Volleyball</a>
+              <a href="#">Cycling</a>
+              <a href="#">Running</a>
+            </div>
+          </li>
+          
+          <li className="dropdown">
+            <button className="dropbtn">Men's Collection</button>
+            <div className="dropdown-content">
+              <a href="#">Men Topwear</a>
+              <a href="#">Men Bottomwear</a>
+              <a href="#">Men Footwear</a>
+              <a href="#">Men Jackets</a>
+            </div>
+          </li>
+
+          <li className="dropdown">
+            <button className="dropbtn">Women's Collection</button>
+            <div className="dropdown-content">
+              <a href="#">Women Topwear</a>
+              <a href="#">Women Bottomwear</a>
+              <a href="#">Women Footwear</a>
+              <a href="#">Women Jackets</a>
+            </div>
+          </li>
+
+          <li className="dropdown">
+            <button className="dropbtn">Kid's Collection</button>
+            <div className="dropdown-content">
+              <a href="#">Kids Topwear</a>
+              <a href="#">Kids Bottomwear</a>
+              <a href="#">Kids Footwear</a>
+              <a href="#">Kids Activewear</a>
+              <a href="#">Kids Accessories</a>
+              <a href="#">Kids Sports Equipment</a>
+            </div>
+          </li>
+        </ul>
+        
+        {/* <ul className="nav-links">
           <li><a href="#">All Sports</a></li>
           <li><a href="#">Men's Collection</a></li>
           <li><a href="#">Women's Collection</a></li>
           <li><a href="#">Kid's Collection</a></li>
-        </ul>
+        </ul>*/}
         <input type="text" placeholder="Search your gear..." className="search-bar" />
-        </div>
-      </nav>
+      </nav> 
 
       {/* Slideshow Section */}
       <div className="slideshow">
@@ -54,12 +94,13 @@ const App = () => {
         {/* Slideshow Text Content */}
         <div className="slide-content">
           <h1>SPORTIFY</h1>
+
           <p>🔥 Fuel Your Passion. Elevate Your Game! 🔥</p>
           
           <a href="/shop" className="shop-now">Shop Now</a>
         </div>
       </div>
-    </div>
+      </div>
   );
 };
 
