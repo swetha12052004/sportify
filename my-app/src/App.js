68c2833d3d28422a1edd7b8806db60a2da8bdfcd
swetha12052004 <swethaswe259@gmail.com>
2025-04-02 23:12:55 +0530

    night

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -10,7 +10,7 @@ function App() {
   return ( 
     <Router>
       <Routes>
-        <Route path="/" element={<Login/>} /> 
+        <Route path="/" element={<Reg/>} /> 
         <Route path="/Product" element={<Product />} /> 
         {/* <Route path="/Home" element={<Home />} /> */}
         <Route path="/Reg" element={<Reg />} />
--- my-app/src/project/Login.jsx
+++ my-app/src/project/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./Login.css";
+import "./Login module.css";
 import timeImage from "../images/gre.jpg";
 import googleIcon from "../images/goo.png";
 import facebookIcon from "../images/face.png";
